893cb55b3e0fc4f2b6056128ca1bce6d292f0921
Osaki Haruki <mikemuramaro@gmail.com>
2025-07-01 16:24:47 +0900

    Won/Lost message added

--- dice.c
+++ dice.c
@@ -8,9 +8,10 @@ srand((unsigned int)time(NULL));
 int num1=rand()%6+1;
 int num2=rand()%6+1;
 int total=num1+num2;
-
 printf("Rolling dice...\n");
 printf("Die1:%d\n",num1);
 printf("Die2:%d\n",num2);
 printf("Total value:%d\n",total);
+if(total>7) printf("You won\n");
+if(total<=7) printf("You lost\n");
 }
